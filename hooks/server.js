b3768f990b473c332bba4a5548f8cd4d9f45a953
Connor Turland <connorturland@gmail.com>
2016-03-17 07:04:17 +1100

    Update server.js

--- hooks/server.js
+++ hooks/server.js
@@ -1,1 +1,22 @@
+require express
+require Post
 
+require Hook
+
+start server
+  app.post('/services/:community_id/:hook_id/:hook_secret', function (req, res) {
+var hook = Hook.find community_id hook_id hook_secret
+
+if not hook res.send 404, return
+
+Check validity of payload
+if not valid res.send 400, return
+
+get params from req
+Post.create(with params)
+
+return res.ok
+
+});
+
+end
